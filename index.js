925bfee3c3f9aca2abe72f5acfe1043b6118443c
Nastassia27 <anastasia.derugo@gmail.com>
2023-08-09 16:01:04 +0300

    2. get data from request

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 
 var a = 5
-$.ajax('https://repetitora.net/api/JS/Images', );
+console.log(0)
+$.ajax('https://rapidapi.com/martin.svoboda/api/quotes15/details', {
+    success: function(date){
+        console.log(data)
+      /* data.forEach(el=>{
+          const img =  document.createElement('img')
+           img.src=el.thumbnail;
+          document.querySelector('body').appendChild(img);*/
+       }
+    });
+console.log(1)
 
-a = 8;
-console.log(a);
+/*a = 8;
+console.log(a);*/
+
+//https://repetitora.net/api/JS/Images
+ //setTimeout(function(){},1000)
 
 
- setTimeout
 
-
-
+/*
 const resultBlock = document.querySelector('#result');
 const clickMeButton = document.querySelector('#click-me');
 clickMeButton.addEventListener('click', makeRequest);
 
 function makeRequest(){
     resultBlock.innerHTML='result'
-}
+}*/
 
